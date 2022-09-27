6a721fe6ed6207610740f1a8ecedfc06c5a5dedc
Ihsanulla-Yasar <ihsanullahyasar70@gmail.com>
2022-09-26 22:19:22 -0700

    worked on responsiveness

---
+++ jscript.js
@@ -0,0 +1,14 @@
+
+        function openNav() {
+            document.querySelector("nav ul").style.display="block";
+            document.getElementById("openNav").style.display = "none";
+            document.getElementById("closeNav").style.display = "inline-block";
+          
+        }
+        
+        function closeNav() {
+            document.querySelector("nav ul").style.display="none";
+            document.getElementById("openNav").style.display = "inline-block";
+            document.getElementById("closeNav").style.display = "none";
+            
+        }
